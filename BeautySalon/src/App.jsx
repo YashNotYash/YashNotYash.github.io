a706d6631f9d0dfda63197641374d4922fb95967
Yash Prajapati <prajapatiyash265@gmail.com>
2025-08-05 12:04:21 -0400

    Initial Commit

---
+++ BeautySalon/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to the Beauty Salon</h1>
+    </>
+  )
+}
+
+export default App
